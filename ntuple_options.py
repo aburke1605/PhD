3a471df6cac68e4cc2aacb58818dda810ac7be04
aburke1605 <aodhan.burke@student.manchester.ac.uk>
2020-10-15 13:10:21 +0200

    .

--- ntuple_options.py
+++ ntuple_options.py
@@ -8,8 +8,36 @@ line = 'D2hhPromptDst2D2KKLine'
 # Create an ntuple to capture D*+ decays from the StrippingLine line
 dtt = DecayTreeTuple('TupleDstToD0pi_D0ToKK')
 dtt.Inputs = ['/Event/{0}/Phys/{1}/Particles'.format(stream, line)] #Phys/{0}/Particles'.format(line)] # for microDST
-dtt.Decay = '[D*(2010)+ -> (D0 -> K- K+) pi+]CC'
+#dtt.Decay = '[D*(2010)+ -> (D0 -> K- K+) pi+]CC' # written like this means tuple tools only runs on the D*(2010)+, to select particles to store information on mark them with a ^, e.g.:
+dtt.Decay = '[D*(2010)+ -> ^(D0 -> ^K- ^K+) ^pi+]CC' # but this not ideal as we may only want to run some tools on D and some on children, use Branches below
+# Additional tuple tools:
+track_tool = dtt.addTupleTool('TupleToolTrackInfo')
+# Only need to store a tool in a variable if you want to configure it:
+track_tool.Verbose = True # 'Verbose' - further information
+dtt.addTupleTool('TupleToolPrimaries')
 
+dtt.addBranches({'Dstar'  :'[D*(2010)+ -> (D0 -> K- K+) pi+]CC',
+                 'D0'     :'[D*(2010)+ -> ^(D0 -> K- K+) pi+]CC',
+                 'Kminus' :'[D*(2010)+ -> (D0 -> ^K- K+) pi+]CC',
+                 'Kplus'  :'[D*(2010)+ -> (D0 -> K- ^K+) pi+]CC',
+                 'pisoft' :'[D*(2010)+ -> (D0 -> K- K+) ^pi+]CC'}) # and mark particles which we want to use in dtt.Decay (with ^ ?)!!
+# Alternativley, use setDescriptorTemplate() to do all this in one line:
+#dtt.setDescriptorTemplate('${Dstar}[D*(2010)+ -> ${D0}(D0 -> ${Kminus}K- ${Kplus}K+) ${pisoft}pi+]CC') # sets up both dtt.Decay and Branches
+# Access branches with dtt.PARTICLENAME
+dtt.D0.addTupleTool('TupleToolPropertime') # e.g. of adding tuple tools to specific particles
+'''
+All tuple tools start with DecayTreeTuple here:  https://gitlab.cern.ch/lhcb/Analysis/tree/master/Phys
+      DecayTreeTuple           for the more general tools;
+      DecayTreeTupleANNPID     for the NeuralNet-based PID tools;
+      DecayTreeTupleDalitz     for Dalitz analysis;
+      DecayTreeTupleJets       for obtaining information on jets;
+      DecayTreeTupleMC         gives us access to MC-level information;
+      DecayTreeTupleMuonCalib  for muon calibration tools;
+      DecayTreeTupleReco       for reconstruction-level information, such as TupleToolTrackInfo;
+      DecayTreeTupleTracking   for more detailed tools regarding tracking;
+      DecayTreeTupleTrigger    for accessing to the trigger information of the candidates.
+All info in src/.h files or in coresponding doxygen
+'''
 
 from Configurables import DaVinci
 from PhysConf.Filters import LoKi_Filters
@@ -30,6 +58,7 @@ DaVinci().Lumi = not DaVinci().Simulation
 DaVinci().EvtMax = -1
 DaVinci().CondDBtag = 'sim-20170721-2-vc-md100'
 DaVinci().DDDBtag = 'dddb-20170721-3'
+
 
 from GaudiConf import IOHelper
 
